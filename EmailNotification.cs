c7fed54fa851be0c78bfd39d3e347dea5f2e2d54
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-10 23:37:57 -0300

    Pre merge

--- EmailNotification.cs
+++ EmailNotification.cs
@@ -5,80 +5,72 @@ using Gamidas.Utils.RabbitMQ.Model;
 using Gamidas.Utils.RabbitMQ.Send;
 
 namespace worker_notification;
-public class EmailNotification : IInvocable
+public class EmailNotification(ILogger<EmailNotification> logger, OracleConnection connection, ISendEvent sendEvent) : IInvocable
 {
-    private readonly ILogger<EmailNotification> _logger;
-    private readonly OracleConnection _connection;
-    private readonly ISendEvent _sendEvent;
+	private readonly ILogger<EmailNotification> _logger = logger;
+	private readonly OracleConnection _connection = connection;
+	private readonly ISendEvent _sendEvent = sendEvent;
 
-    public EmailNotification(ILogger<EmailNotification> logger, OracleConnection connection, ISendEvent sendEvent)
-    {
-        _logger = logger;
-        _connection = connection;
-        _sendEvent = sendEvent;
-    }
+	public Task Invoke()
+	{
+		DateTime dateTimeNow = DateTime.UtcNow.AddHours(-3);
 
-    public Task Invoke()
-    {
-        DateTime dateTimeNow = DateTime.UtcNow.AddHours(-3);
+		IEnumerable<NotificationSettingMapper> notifications = _connection.Query<NotificationSettingMapper>("SELECT * FROM NOTIFICATIONS_SETTINGS");
 
-        IEnumerable<NotificationSettingMapper> notifications = _connection.Query<NotificationSettingMapper>("SELECT * FROM NOTIFICATIONS_SETTINGS");
+		_logger.LogInformation("Found {notifications} e-mails to send.", notifications.Count());
 
-        _logger.LogInformation($"Found {notifications.Count()} e-mails to send.");
+		foreach (NotificationSettingMapper notification in notifications)
+		{
+			_logger.LogInformation("Starting {notification}.", notification.ID);
 
-        foreach (NotificationSettingMapper notification in notifications)
-        {
-            _logger.LogInformation($"Starting {notification.ID}.");
+			if (dateTimeNow > notification.DUE_DATE)
+			{
+				_connection.Execute("DELETE FROM NOTIFICATIONS_SETTINGS WHERE ID = :id", new { id = notification.ID });
 
-            if (dateTimeNow > notification.DUE_DATE)
-            {
-                _connection.Execute("DELETE FROM NOTIFICATIONS_SETTINGS WHERE ID = :id", new { id = notification.ID });
+				_logger.LogInformation("Notification {notification} deleted successfully.", notification.ID);
 
-                _logger.LogInformation($"Notification {notification.ID} deleted successfully.");
+				continue;
+			}
 
-                continue;
-            }
+			if ((notification.DUE_DATE - dateTimeNow).TotalDays > 30)
+			{
+				_logger.LogInformation("Notification {notification} does not passed the validation.", notification.ID);
+				continue;
+			}
 
-            if ((notification.DUE_DATE - dateTimeNow).TotalDays > 30)
-            {
-                _logger.LogInformation($"Notification {notification.ID} does not passed the validation.");
-                continue;
-            }
+			decimal daysLeft = (notification.DUE_DATE - dateTimeNow).Days;
+			decimal hoursLeft = (notification.DUE_DATE - dateTimeNow).Hours;
 
-            decimal yearsLeft = (notification.DUE_DATE - dateTimeNow).Days / 365;
-            decimal daysLeft = (notification.DUE_DATE - dateTimeNow).Days;
-            decimal hoursLeft = (notification.DUE_DATE - dateTimeNow).Hours;
+			string subject = $"Worker Notification | {notification.SUBJECT}";
+			string body = notification.BODY
+				+ $"\r\n\r\n--------------------------------------------------------------"
+				+ $"\r\n\r\nRestam: {daysLeft} dias e {hoursLeft} horas."
+				+ $"\r\n\r\nData final: {notification.DUE_DATE:dd/MM/yyyy}";
 
-            string subject = $"Worker Notification | {notification.SUBJECT}";
-            string body = notification.BODY
-                + $"\r\n\r\n--------------------------------------------------------------"
-                + $"\r\n\r\nRestam: {yearsLeft} anos, {daysLeft} dias e {hoursLeft} horas."
-                + $"\r\n\r\nData final: {notification.DUE_DATE:dd/MM/yyyy}";
+			EmailModel email = new(notification.RECIPIENT, body, subject);
 
-            EmailModel email = new(notification.RECIPIENT, body, subject);
+			_logger.LogInformation($"Email model created: {subject}.");
 
-            _logger.LogInformation($"Email model created: {subject}.");
+			try
+			{
+				_sendEvent.SendEmail(email);
+				_logger.LogInformation("E-mail {notification} published successfully.", notification.ID);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Error while publishing email: {message}", ex.Message);
+			}
+		}
 
-            try
-            {
-                _sendEvent.SendEmail(email);
-                _logger.LogInformation($"E-mail {notification.ID} published successfully.");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error while publishing email: {ex.Message}");
-            }
-        }
+		return Task.CompletedTask;
+	}
 
-        return Task.CompletedTask;
-    }
-
-    class NotificationSettingMapper
-    {
-        public int ID { get; set; }
-        public string RECIPIENT { get; set; }
-        public string SUBJECT { get; set; }
-        public string BODY { get; set; }
-        public DateTime DUE_DATE { get; set; }
-    }
+	class NotificationSettingMapper
+	{
+		public int ID { get; set; }
+		public string RECIPIENT { get; set; }
+		public string SUBJECT { get; set; }
+		public string BODY { get; set; }
+		public DateTime DUE_DATE { get; set; }
+	}
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using worker_notification;
 using Coravel;
 using Oracle.ManagedDataAccess.Client;
 using Gamidas.Utils;
+using Serilog;
+using Serilog.Events;
 
 HostBuilder builder = new();
 builder.ConfigureAppConfiguration((hostingContext, config) =>
@@ -10,14 +12,26 @@ builder.ConfigureAppConfiguration((hostingContext, config) =>
 	config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 });
 
+string connectionString = "";
+
 IHost host = builder.ConfigureServices((hostContext, services) =>
 {
+	connectionString = hostContext.Configuration.GetConnectionString("Default");
 	services.ConfigureGamidas();
-	services.AddTransient(x => new OracleConnection(hostContext.Configuration.GetConnectionString("Default")));
+	services.AddTransient(x => new OracleConnection(connectionString));
+	services.AddSerilog();
 	services.AddScheduler();
 	services.AddTransient<EmailNotification>();
-})
-	.Build();
+}).Build();
+
+Log.Logger = new LoggerConfiguration()
+	.Enrich.WithProperty("ApplicationName", "WORKER-Notification")
+	.MinimumLevel.Warning()
+	.MinimumLevel.Override("worker_notification", LogEventLevel.Information)
+	.WriteTo.Oracle(cfg => cfg.WithSettings(connectionString)
+		.UseBurstBatch()
+		.CreateSink())
+	.CreateLogger();
 
 host.Services.UseScheduler(scheduler =>
 {
